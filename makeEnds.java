1c95600c614faf5f2e8dfa792742f2d626751ed0
Saimon Ahmed <67533318+SAIMON-AHMED@users.noreply.github.com>
2021-10-09 15:00:06 +0600

    Created makeEnds.java

---
+++ makeEnds.java
@@ -0,0 +1,17 @@
+/*
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+
+
+makeEnds([1, 2, 3]) → [1, 3]
+makeEnds([1, 2, 3, 4]) → [1, 4]
+makeEnds([7, 4, 6, 2]) → [7, 2]
+*/
+
+public int[] makeEnds(int[] nums) {
+  int[] array = new int[2];
+  if (nums.length >= 1) {
+    array[0] = nums[0];
+    array[1] = nums[nums.length-1];
+  }
+  return array;
+}
